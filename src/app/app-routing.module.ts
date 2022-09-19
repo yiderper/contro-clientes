1909bd2fcaa85e101b8e4d71ee1634708d12efbf
yiderper <yider.per@gmail.com>
2022-09-18 22:37:05 -0500

    Agrega el Proyecto Control Clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TableroComponent}, // Ruta por default
+  {path: 'login', component: LoginComponent},
+  {path: 'registrarse', component: RegistroComponent},
+  {path: 'configuracion', component: ConfiguracionComponent},
+  {path: 'cliente/editar/:id', component: EditarClienteComponent},
+  {path: '**', component: NoEncontradoComponent}, //Rutas que no se encuentran
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
